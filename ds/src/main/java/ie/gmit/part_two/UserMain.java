640dccb7cee1757b9030b7f9681af7afba2a627b
farisNassif <g00347032@gmit.ie>
2019-11-15 13:25:45 +0000

    work started on part 2

---
+++ ds/src/main/java/ie/gmit/part_two/UserMain.java
@@ -0,0 +1,12 @@
+package ie.gmit.part_two;
+
+public class UserMain {
+
+	public static void main(String[] args) throws Exception {
+		new UserMain().run();
+	}   
+	
+	public void run() throws Exception {
+
+    }
+}
