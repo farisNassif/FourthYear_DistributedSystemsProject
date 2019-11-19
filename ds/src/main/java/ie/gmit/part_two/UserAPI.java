074b58bbe80a55feb01825b18c8df1db151805a6
farisNassif <g00347032@gmit.ie>
2019-11-19 19:25:45 +0000

    get/post working with postman

--- ds/src/main/java/ie/gmit/part_two/UserAPI.java
+++ ds/src/main/java/ie/gmit/part_two/UserAPI.java
@@ -1,26 +1,43 @@
 package ie.gmit.part_two;
 
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/users")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class UserAPI {
-
-	List<User> users = Arrays.asList(new User(1, "joe", "joe@gmail.com", "pw"));
-
+	
+	private HashMap<Integer, User> userMap = new HashMap<>();
+	
+    public UserAPI() {     
+    	User plsWork = new User(1, "joe", "bigjoe@gmail.com", "secretpw");
+    	userMap.put(plsWork.getId(), plsWork);
+    }
+	
 	@GET
 	public List<User> getUsers() {
 
-		return users;
+		return new ArrayList<User>(userMap.values());
 	}
+	
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response add(User newUser) {
+
+        //System.out.println("NEW" + newUser.getUserId());
+        userMap.put(newUser.getId(), newUser);
+
+        String r = "user id: " + newUser.getId();
+        return Response.status(201).entity(r).build();
+    }
 }
